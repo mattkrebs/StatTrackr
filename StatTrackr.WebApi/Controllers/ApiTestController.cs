169c175d49b0a65273dbbf28fc7bf0c6592492b9
Matt Krebs <matt.krebs@outlook.com>
2015-03-11 12:49:49 -0500

    add test page

---
+++ StatTrackr.WebApi/Controllers/ApiTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StatTrackr.WebApi.Controllers
+{
+    public class ApiTestController : Controller
+    {
+        // GET: ApiTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
